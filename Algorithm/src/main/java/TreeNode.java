90027f33af8bb17668bc488636f98f2f010a9bce
jiatengyang <tengyangjia@gmail.com>
2018-06-14 17:58:53 +0800

    Binary Tree

---
+++ Algorithm/src/main/java/TreeNode.java
@@ -0,0 +1,54 @@
+
+/**
+ * Copyright (C) 2014 iQIYI.COM - All Rights Reserved
+ * <p>
+ * Unauthorized copy of this file, via any medium is strictly prohibited.
+ * Proprietary and Confidential.
+ *
+ * @description: The tree node class used to build tree data structure
+ * @author: Bangkura
+ * @create: 2018-06-14 11:57
+ **/
+
+public class TreeNode<T extends Comparable<T>> {
+    private T val;
+    private TreeNode<T> left = null;
+    private TreeNode<T> right = null;
+    private TreeNode<T> parent = null;
+
+    public TreeNode(T val) {
+        this.val = val;
+    }
+
+    public T getVal() {
+        return val;
+    }
+
+    public void setVal(T val) {
+        this.val = val;
+    }
+
+    public TreeNode<T> getLeft() {
+        return left;
+    }
+
+    public void setLeft(TreeNode<T> left) {
+        this.left = left;
+    }
+
+    public TreeNode<T> getRight() {
+        return right;
+    }
+
+    public void setRight(TreeNode<T> right) {
+        this.right = right;
+    }
+
+    public TreeNode<T> getParent() {
+        return parent;
+    }
+
+    public void setParent(TreeNode<T> parent) {
+        this.parent = parent;
+    }
+}
